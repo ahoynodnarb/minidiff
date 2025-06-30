3df009bb32ce363ec70f1d42c08a7cd537376da5
Brandon Yao <ahoynodnarb@gmail.com>
2025-06-30 04:01:58 -0700

    Increase leniency for trig and hyperbolic trig functions in unit testing

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -48,418 +48,455 @@ def perform_test(
 
 
 def test_ravel():
-    perform_test(
-        func=md.ravel,
-        backend_func=lambda x: x.ravel(),
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.ravel,
+            backend_func=lambda x: x.ravel(),
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_flatten():
-    perform_test(
-        func=md.flatten,
-        backend_func=lambda x: x.flatten(),
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.flatten,
+            backend_func=lambda x: x.flatten(),
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_squeeze():
-    perform_test(
-        func=md.squeeze,
-        backend_func=np.squeeze,
-        args=[
-            md.randn(1, 2, 1, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.squeeze,
+            backend_func=np.squeeze,
+            args=[
+                md.randn(1, 2, 1, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_expand_dims():
-    perform_test(
-        func=md.expand_dims,
-        backend_func=np.expand_dims,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            tuple(random.sample(range(4), k=random.randint(0, 4))),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.expand_dims,
+            backend_func=np.expand_dims,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                tuple(random.sample(range(4), k=random.randint(0, 4))),
+            ],
+            kwargs={},
+        )
 
 
 def test_max():
-    perform_test(
-        func=md.max,
-        backend_func=np.max,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={"axis": random.randint(0, 3)},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.max,
+            backend_func=np.max,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={"axis": random.randint(0, 3)},
+        )
 
 
 def test_where():
-    perform_test(
-        func=md.where,
-        backend_func=np.where,
-        args=[
-            md.binomial(1, random.uniform(0, 1), (2, 2, 2, 2)),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.where,
+            backend_func=np.where,
+            args=[
+                md.binomial(1, random.uniform(0, 1), (2, 2, 2, 2)),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_prod():
-    perform_test(
-        func=md.prod,
-        backend_func=np.prod,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={"axis": tuple(random.sample(range(4), k=random.randint(0, 4)))},
-        backward_rtol=1e-01,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.prod,
+            backend_func=np.prod,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={"axis": tuple(random.sample(range(4), k=random.randint(0, 4)))},
+            backward_rtol=1e-01,
+        )
 
 
 def test_transpose():
-    perform_test(
-        func=md.transpose,
-        backend_func=np.transpose,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={"axes": md.permutation(range(4))},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.transpose,
+            backend_func=np.transpose,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={"axes": md.permutation(range(4))},
+        )
 
 
 def test_swapaxes():
-    perform_test(
-        func=md.swapaxes,
-        backend_func=np.swapaxes,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            random.randint(0, 3),
-            random.randint(0, 3),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.swapaxes,
+            backend_func=np.swapaxes,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                random.randint(0, 3),
+                random.randint(0, 3),
+            ],
+            kwargs={},
+        )
 
 
 def test_flip():
-    perform_test(
-        func=md.flip,
-        backend_func=np.flip,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={"axis": random.randint(0, 3)},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.flip,
+            backend_func=np.flip,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={"axis": random.randint(0, 3)},
+        )
 
 
 def test_dot():
-    perform_test(
-        func=md.dot,
-        backend_func=np.dot,
-        args=[
-            md.randn(2, allow_grad=True),
-            md.randn(2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.dot,
+            backend_func=np.dot,
+            args=[
+                md.randn(2, allow_grad=True),
+                md.randn(2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_broadcast_to():
-    perform_test(
-        func=md.broadcast_to,
-        backend_func=np.broadcast_to,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            (4, 2, 2, 2, 2),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.broadcast_to,
+            backend_func=np.broadcast_to,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                (4, 2, 2, 2, 2),
+            ],
+            kwargs={},
+        )
 
 
 def test_atleast_1d():
-    perform_test(
-        func=md.atleast_1d,
-        backend_func=np.atleast_1d,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.atleast_1d,
+            backend_func=np.atleast_1d,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_atleast_2d():
-    perform_test(
-        func=md.atleast_2d,
-        backend_func=np.atleast_2d,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.atleast_2d,
+            backend_func=np.atleast_2d,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_atleast_3d():
-    perform_test(
-        func=md.atleast_3d,
-        backend_func=np.atleast_3d,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.atleast_3d,
+            backend_func=np.atleast_3d,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_copy():
-    perform_test(
-        func=md.copy,
-        backend_func=np.copy,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.copy,
+            backend_func=np.copy,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_getitem():
-    perform_test(
-        func=md.getitem,
-        backend_func=lambda x, key: x[key],
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randint(low=(0, 0, 0, 0), high=(2, 2, 2, 2)),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.getitem,
+            backend_func=lambda x, key: x[key],
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randint(low=(0, 0, 0, 0), high=(2, 2, 2, 2)),
+            ],
+            kwargs={},
+        )
 
 
 def test_clip():
-    perform_test(
-        func=md.clip,
-        backend_func=np.clip,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            random.uniform(0, 10),
-            random.uniform(-10, 0),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.clip,
+            backend_func=np.clip,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                random.uniform(0, 10),
+                random.uniform(-10, 0),
+            ],
+            kwargs={},
+        )
 
 
 def test_reshape():
-    perform_test(
-        func=md.reshape,
-        backend_func=np.reshape,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            (4, 4),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.reshape,
+            backend_func=np.reshape,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                (4, 4),
+            ],
+            kwargs={},
+        )
 
 
 def test_matmul():
-    perform_test(
-        func=md.matmul,
-        backend_func=np.matmul,
-        args=[
-            md.randn(2, 2, allow_grad=True),
-            md.randn(2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.matmul,
+            backend_func=np.matmul,
+            args=[
+                md.randn(2, 2, allow_grad=True),
+                md.randn(2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_tensordot():
-    perform_test(
-        func=md.tensordot,
-        backend_func=np.tensordot,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.tensordot,
+            backend_func=np.tensordot,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_add():
-    perform_test(
-        func=md.add,
-        backend_func=np.add,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.add,
+            backend_func=np.add,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_subtract():
-    perform_test(
-        func=md.subtract,
-        backend_func=np.subtract,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.subtract,
+            backend_func=np.subtract,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_multiply():
-    perform_test(
-        func=md.multiply,
-        backend_func=np.multiply,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.multiply,
+            backend_func=np.multiply,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_true_divide():
-    perform_test(
-        func=md.true_divide,
-        backend_func=np.true_divide,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-        backward_rtol=1e-01,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.true_divide,
+            backend_func=np.true_divide,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-05,
+        )
 
 
 def test_power():
-    perform_test(
-        func=md.power,
-        backend_func=np.power,
-        args=[
-            md.randn(2, 2, 2, 2, allow_grad=True),
-            md.randn(2, 2, 2, 2, allow_grad=True),
-        ],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.power,
+            backend_func=np.power,
+            args=[
+                md.randn(2, 2, 2, 2, allow_grad=True),
+                md.randn(2, 2, 2, 2, allow_grad=True),
+            ],
+            kwargs={},
+        )
 
 
 def test_cos():
-    perform_test(
-        func=md.cos,
-        backend_func=np.cos,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-        backward_rtol=1e-02,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.cos,
+            backend_func=np.cos,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-04,
+        )
 
 
 def test_sin():
-    perform_test(
-        func=md.sin,
-        backend_func=np.sin,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-        backward_rtol=1e-02,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.sin,
+            backend_func=np.sin,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-04,
+        )
 
 
 def test_tan():
-    perform_test(
-        func=md.tan,
-        backend_func=np.tan,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-        backward_rtol=1e-02,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.tan,
+            backend_func=np.tan,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-04,
+        )
 
 
 def test_cosh():
-    perform_test(
-        func=md.cosh,
-        backend_func=np.cosh,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-        backward_rtol=1e-02,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.cosh,
+            backend_func=np.cosh,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-04,
+        )
 
 
 def test_sinh():
-    perform_test(
-        func=md.sinh,
-        backend_func=np.sinh,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-        backward_rtol=1e-02,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.sinh,
+            backend_func=np.sinh,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-04,
+        )
 
 
 def test_tanh():
-    perform_test(
-        func=md.tanh,
-        backend_func=np.tanh,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-        backward_rtol=1e-02,
-        backward_atol=1e-05,
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.tanh,
+            backend_func=np.tanh,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+            backward_rtol=1e-01,
+            backward_atol=1e-04,
+        )
 
 
 def test_exp():
-    perform_test(
-        func=md.exp,
-        backend_func=np.exp,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.exp,
+            backend_func=np.exp,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+        )
 
 
 def test_log():
-    perform_test(
-        func=md.log,
-        backend_func=np.log,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.log,
+            backend_func=np.log,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+        )
 
 
 def test_sum():
-    perform_test(
-        func=md.sum,
-        backend_func=np.sum,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.sum,
+            backend_func=np.sum,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+        )
 
 
 def test_mean():
-    perform_test(
-        func=md.mean,
-        backend_func=np.mean,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.mean,
+            backend_func=np.mean,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+        )
 
 
 def test_absolute():
-    perform_test(
-        func=md.absolute,
-        backend_func=np.absolute,
-        args=[md.randn(2, 2, 2, 2, allow_grad=True)],
-        kwargs={},
-    )
+    for _ in range(5):
+        perform_test(
+            func=md.absolute,
+            backend_func=np.absolute,
+            args=[md.randn(2, 2, 2, 2, allow_grad=True)],
+            kwargs={},
+        )
