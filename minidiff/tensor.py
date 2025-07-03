cfb9a7f78174aa7e7e5c483813532dcdd5d6b619
Brandon Yao <ahoynodnarb@gmail.com>
2025-07-03 16:49:36 -0700

    Set cleanup_mode to prune for higher order derivatives instead of keep

--- minidiff/tensor.py
+++ minidiff/tensor.py
@@ -184,7 +184,7 @@ class Tensor:
         # will almost always be necessary so those have to be kept in memory too
         if allow_higher_order:
             retain_grads = True
-            cleanup_mode = "keep"
+            cleanup_mode = "prune"
 
         if reset_grads:
             for tensor in traversal_path:
