748f96124441fcd013e696658863a669482f8b14
Brandon Yao <ahoynodnarb@gmail.com>
2025-08-31 21:58:12 -0500

    Refactored backend import code

--- minidiff/__init__.py
+++ minidiff/__init__.py
@@ -1,2 +1,6 @@
+from .backend import attempt_backend_import
+
+attempt_backend_import()
+
 from .ops.definitions import *
 from .tensor import *
--- minidiff/backend/__init__.py
+++ minidiff/backend/__init__.py
@@ -1,9 +1,29 @@
+from __future__ import annotations
+
 import importlib
+from argparse import ArgumentParser
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+parser = ArgumentParser()
+parser.add_argument(
+    "--backend", help="specify selected backend", required=False, default=None
+)
+args = vars(parser.parse_args())
+
+SPECIFIED_BACKEND = args["backend"]
+DEFAULT_BACKENDS = [
+    "minidiff.backend.cupy",
+    "minidiff.backend.mlx",
+    "minidiff.backend.numpy",
+]
 
 
-def import_selected_backend(selected_backend, package_name=None):
+def import_backend(backend_name: str, package_name: Optional[str] = None):
     # https://stackoverflow.com/questions/43059267/how-to-do-from-module-import-using-importlib
-    module = importlib.import_module(selected_backend, package=package_name)
+    module = importlib.import_module(backend_name, package=package_name)
     module_dict = module.__dict__
     if "__all__" in module_dict:
         module_exports = module_dict["__all__"]
@@ -13,28 +33,32 @@ def import_selected_backend(selected_backend, package_name=None):
     globals().update({k: getattr(module, k) for k in module_exports})
 
 
-found_backend = False
+def attempt_backend_import():
+    used_backend = None
 
-if not found_backend:
-    try:
-        import_selected_backend("minidiff.backend.cupy")
-        found_backend = True
-    except ImportError:
-        pass
+    def attempt_import(possible_backend: Optional[str]) -> bool:
+        if possible_backend is None:
+            return False
 
-if not found_backend:
-    try:
-        import_selected_backend("minidiff.backend.mlx")
-        found_backend = True
-    except ImportError:
-        pass
+        try:
+            import_backend(possible_backend)
+            return True
+        except:
+            return False
 
-if not found_backend:
-    try:
-        import_selected_backend("minidiff.backend.numpy")
-        found_backend = True
-    except ImportError:
-        pass
+    if attempt_import(SPECIFIED_BACKEND):
+        used_backend = SPECIFIED_BACKEND
 
-if not found_backend:
-    raise Exception("could not find a suitable backend")
+    if used_backend is None:
+        for possible_backend in DEFAULT_BACKENDS:
+            if attempt_import(possible_backend):
+                used_backend = possible_backend
+                break
+
+    if used_backend is None:
+        raise Exception("could not find a suitable backend")
+
+    if SPECIFIED_BACKEND is not None and SPECIFIED_BACKEND != used_backend:
+        print(
+            f"could not find backend named {SPECIFIED_BACKEND}, defaulting to {used_backend} instead"
+        )
